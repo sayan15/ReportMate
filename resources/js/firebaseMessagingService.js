5c917d933e7e7c76facdb97db2e5dc98d5c283d0
sayan15 <sayansaya95@gmail.com>
2024-01-08 00:19:08 +0000

    changes made in send notification

--- resources/js/firebaseMessagingService.js
+++ resources/js/firebaseMessagingService.js
@@ -2,10 +2,15 @@
 import { getMessaging, getToken } from "firebase/messaging";
 
 
-$(document).ready(function(){
-    initFirebaseMessagingRegistration();
-    showAlert();
-})
+
+async function initMap() {
+  // Request the needed libraries.
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
+
+  // Load the Autocomplete feature once the libraries are ready.
+  autoLocationLoader();
+}
 
 var key="BN7z12WZZq-wdTDhkmvN82p98pfqx6t2zG6-gOJSND0-XwdUu7r5LAs_FyD8oxhTAzQkZqdW9PYdbj6ZxrZe_4E";
 function initFirebaseMessagingRegistration() {
@@ -56,5 +61,38 @@ function showAlert(){
 }
 
 
+// Define a global variable to hold the Autocomplete object.
+let userLocation;
 
-;
+
+function autoLocationLoader() {
+    // Get userLocation
+        // Ensure the google.maps object is defined before accessing Autocomplete.
+        if (typeof google !== 'undefined' && google.maps && google.maps.places) {
+            userLocation = new google.maps.places.Autocomplete(document.getElementById('locationInput'));
+            // Listen for the 'place_changed' event when a place is selected in the Autocomplete input.
+            userLocation.addListener('place_changed', function() {
+                // Get the selected place from the Autocomplete input.
+                const place = userLocation.getPlace();
+
+                if (place.geometry) {
+                    // Get the latitude and longitude from the selected place.
+                    const lat = place.geometry.location.lat();
+                    const lng = place.geometry.location.lng();
+
+                    // Now, 'lat' and 'lng' contain the latitude and longitude of the selected place.
+                    document.getElementById('latitudeInput').value=lat;
+                    document.getElementById('longitudeInput').value=lng;
+                }
+            });
+        }
+        
+}
+
+$(document).ready(function(){
+  initFirebaseMessagingRegistration();
+  showAlert();
+  initMap();
+})
+
+
